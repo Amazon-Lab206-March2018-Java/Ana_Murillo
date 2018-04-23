4fa3f1972e52cd377257a13f90a7e46b353ae4f7
Murillo Rojas <anaroja@amazon.com>
2018-04-23 15:15:37 -0600

    first commit

---
+++ javaFun/PythagoreanTest.java
@@ -0,0 +1,6 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean calculateHypotenuse = new Pythagorean(); 
+        System.out.println(calculateHypotenuse.calculateHypotenuse(4,6));
+    }
+}
